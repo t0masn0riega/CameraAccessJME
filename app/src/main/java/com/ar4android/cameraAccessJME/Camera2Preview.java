c4628f897c0326e44215d9fc83a8fc78f29d84a3
Tomas Noriega <tomas.noriega@ca.com>
2016-02-08 14:45:59 -0600

    Initial checkin

---
+++ app/src/main/java/com/ar4android/cameraAccessJME/Camera2Preview.java
@@ -0,0 +1,71 @@
+/* CameraPreview - CameraAccessJME Example
+ * 
+ * Example Chapter 2
+ * accompanying the book
+ * "Augmented Reality for Android Application Development", Packt Publishing, 2013.
+ * 
+ * Copyright  2013 Jens Grubert, Raphael Grasset / Packt Publishing.
+ * 
+ */
+package com.ar4android.cameraAccessJME;
+
+import android.app.Activity;
+import android.content.Context;
+import android.hardware.Camera;
+import android.hardware.camera2.CameraManager;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.Toast;
+
+public class Camera2Preview extends SurfaceView implements SurfaceHolder.Callback {
+    private static final String TAG = "CameraPreview";
+	private SurfaceHolder mHolder;
+
+    private Camera2Util mCamera2Util;
+    private Activity mActivity;
+
+    public Camera2Preview(Context context) {
+        super(context);
+        // Install a SurfaceHolder.Callback so we get notified when the
+        // underlying surface is created and destroyed.
+        mHolder = getHolder();
+        mHolder.addCallback(this);
+    }
+
+    public void surfaceCreated(SurfaceHolder holder) {
+        Log.i(TAG, " ***** surfaceCreated - mActivity:[" + mActivity + "] holder.getSurface():" + holder.getSurface() + "]");
+        mCamera2Util = new Camera2Util((CameraManager) mActivity.getSystemService(Context.CAMERA_SERVICE), mMessageHandler, mActivity.getWindowManager().getDefaultDisplay(), holder.getSurface());
+    }
+
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        Log.i(TAG, " ***** surfaceCreated - surfaceDestroyed");
+        mCamera2Util.closeCamera();
+    }
+
+    public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
+        // If your preview can change or rotate, take care of those events here.
+        // Make sure to stop the preview before resizing or reformatting it.
+
+        Log.i(TAG, " ***** surfaceChanged - surfaceChanged");
+        // If your preview can change or rotate, take care of those events here.
+        // Make sure to stop the preview before resizing or reformatting it.
+        if (mHolder.getSurface() == null){
+            // preview surface does not exist
+            return;
+        }
+        mCamera2Util.openCamera(w, h);
+
+    }
+
+    private Handler mMessageHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            Toast.makeText(mActivity, (String) msg.obj, Toast.LENGTH_SHORT).show();
+        }
+    };
+
+
+}
